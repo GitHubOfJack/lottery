3994a0aa97cab765ea2a0dfe64fe634f26e9f6a4
zhaoma3 <zhaoma3@creditease.cn>
2018-01-12 19:14:41 +0800

    提价双色球抓取接口

--- src/test/java/com/jack/lottery/task/FetchHistory.java
+++ src/test/java/com/jack/lottery/task/FetchHistory.java
@@ -1,7 +1,9 @@
 package com.jack.lottery.task;
 
+import com.jack.lottery.dao.LotteryTermDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,6 +12,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
         "classpath:spring/spring-context.xml"
 })
 public class FetchHistory {
+
+    @Autowired
+    private LotteryTermDao lotteryTermDao;
+
     @Test
     public void ssq() {
         System.out.println("ssq");
