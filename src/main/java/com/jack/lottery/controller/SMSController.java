1123bbc2888b578de9845e7927143a91091dd313
zhaoma3 <zhaoma3@creditease.cn>
2018-01-16 11:52:51 +0800

    提交手机验证码接口

--- src/main/java/com/jack/lottery/controller/SMSController.java
+++ src/main/java/com/jack/lottery/controller/SMSController.java
@@ -1,9 +1,14 @@
 package com.jack.lottery.controller;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.jack.lottery.service.SMSService;
 import com.jack.lottery.utils.LotteryStringUtil;
+import com.jack.lottery.utils.exception.BaseException;
 import com.jack.lottery.utils.exception.Exception2ResponseUtils;
 import com.jack.lottery.utils.exception.ParamException;
+import com.jack.lottery.utils.exception.SystermException;
 import com.jack.lottery.vo.CommonResponose;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 @RequestMapping("/sms")
 public class SMSController {
 
     @Autowired
     private SMSService smsService;
+
+    private LoadingCache<String, Integer> mobileCount = CacheBuilder.newBuilder()
+            .refreshAfterWrite(5, TimeUnit.MINUTES)
+            .expireAfterAccess(5, TimeUnit.MINUTES)
+            .maximumSize(10000)
+            .build(new CacheLoader<String, Integer>() {
+                @Override
+                public Integer load(String s) throws Exception {
+                    return 1;
+                }
+            });
 
     /**
      * 发送验证码短信
@@ -33,7 +52,7 @@ public class SMSController {
         }
     }
 
-    private void validateSendMsg(String mobile, int type) throws ParamException {
+    private void validateSendMsg(String mobile, int type) throws BaseException {
         if (StringUtils.isBlank(mobile)) {
             throw new ParamException("用户手机号不存在,mobile:"+mobile);
         }
@@ -42,6 +61,14 @@ public class SMSController {
         }
         if (0 != type && 1 != type) {
             throw new ParamException("type不正确");
+        }
+        try {
+            int num = mobileCount.get(mobile).intValue();
+            if (num <= 5) {
+                mobileCount.put(mobile, num++);
+            }
+        } catch (ExecutionException e) {
+            throw new SystermException("手机次数取值错误");
         }
     }
 }
